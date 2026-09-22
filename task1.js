6157dff41f5a8f1da78f03e66c6fc0b059945873
Boncho <juand_w@hotmail.com>
2026-09-22 12:56:01 -0500

    envío ejercicios 1-4 con test ok

--- task1.js
+++ task1.js
@@ -2,7 +2,9 @@ const arr = []
 
 // ¡Escribe tu código debajo de esta línea!
 
-
+for (let i=1;i<=20;i++){
+    arr.push(i);
+}
 
 // ¡Escribe tu código encima de esta línea!
 
--- task4.js
+++ task4.js
@@ -2,6 +2,13 @@ const arr = [1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 
 // ¡Escribe tu código debajo de esta línea!
 
+for (let i=0; i<=arr.length;i++){
+    for (let j=i+1; j<arr.length;j++){
+        if (arr[i]===arr[j]){
+            arr.splice(j,1);
+        }
+    }
+}
 
 
 // ¡Escribe tu código encima de esta línea!
